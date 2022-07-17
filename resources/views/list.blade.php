805e16be790af08f1379cbc545271bd886b0804f
thanh loc <locnt@nal.vn>
2022-07-17 11:55:13 +0700

    fix image list page

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,7 +4,7 @@
     <div class = "list row">
         <a class = "element col-md-4 col-sm-6 col-xs-6" href = "/as3100">
             <div class = "card" style = "width: 18rem">
-                <img src = "{{ asset('as3100/AS3100.jpg') }}" class = "card-img-top" alt = "AS-3100"/>
+                <img src = "{{ asset('as3100-img/AS3100.jpg') }}" class = "card-img-top" alt = "AS-3100"/>
                 <div class = "card-body">
                     <h2 class = "card-title">Ghế massage thông minh: AS-3100</h2>
                 </div>
@@ -12,7 +12,7 @@
         </a>
         <a class = "element col-md-4 col-sm-6 col-xs-6" href = "/as10000">
             <div class = "card" style = "width: 18rem">
-                <img src = "{{ asset('as10000/AS10000.jpg') }}" class = "card-img-top" alt = "AS-10.000"/>
+                <img src = "{{ asset('as10000-img/AS10000.jpg') }}" class = "card-img-top" alt = "AS-10.000"/>
                 <div class = "card-body">
                     <h2 class = "card-title">Ghế massage thông minh: AS-10.000</h2>
                 </div>
@@ -20,7 +20,7 @@
         </a>
         <a class = "element col-md-4 col-sm-6 col-xs-6" href = "/as2022">
             <div class = "card" style = "width: 18rem">
-                <img src = "{{ asset('as2022/AS2022.jpg') }}" class = "card-img-top" alt = "AS2022"/>
+                <img src = "{{ asset('as2022-img/AS2022.jpg') }}" class = "card-img-top" alt = "AS2022"/>
                 <div class = "card-body">
                     <h2 class = "card-title">Ghế massage thông minh: AS-2022</h2>
                 </div>
